9f0605b2cbe43cd1ebea4832dd81a63e598a4bd8
dimasasna <dimas.asna@gmail.com>
2023-07-06 01:45:47 +0700

    kelar

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
         <ul class="navbar-navi">
             <li><a href="#home">Home</a> </li>
             <li><a href="#search">Status</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Gallery</a></li>
-            <li><a href="#">Testimonials</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#service">Services</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#testimonial">Testimonials</a></li>
+            <li><a href="#contact">Contact</a></li>
         </ul>
         <div class="navbar-extra">
             <div class="navbar-extra-menu" id="hamburger-menu"><i class="ri-menu-line"></i></div>
@@ -47,13 +47,12 @@
     <!-- Status Start -->
     <section class="search" id="search">
         <h1> <span>CLEANING</span> STATUS</h1>
-
+        <hr>
         <div class="row">
             <div class="about-img">
                 <img src="assets/img/logo.png" alt="">
             </div>
             <div class="content">
-
                 <form action="#">
                     <h5>Masukan Kode Invoice</h5>
                     <div class="input-group">
@@ -66,9 +65,136 @@
                 </form>
             </div>
         </div>
+        </div>
     </section>
+
     <!-- Status end -->
+
+    <!-- Service Start -->
+    <section class="service" id="service">
+        <h1>SHOES <span>SERVICES</span> </h1>
+        <hr>
+        <p>Kami telah mencuci lebih dari 1000 pasang sepatu, dan akan terus bertambah! </p>
+        <p> Jasa yang kami tawarkan:</p>
+        <div class="row">
+            <div class="service-card">
+                <img src="assets/img/service1.jpg" alt="Repair">
+                <img src="assets/img/service2.jpg" alt="Repair">
+                <img src="assets/img/service3.jpg" alt="Repair">
+                <img src="assets/img/service2.jpg" alt="Repair">
+                <img src="assets/img/service3.jpg" alt="Repair">
+                <img src="assets/img/service1.jpg" alt="Repair">
+            </div>
+        </div>
+    </section>
+    <!-- Service End -->
+
+    <!-- Gallery Start -->
+    <section class="gallery" id="gallery">
+        <h1>GALLERY</h1>
+        <hr>
+        <div class="row">
+            <div class="gallery-card">
+                <img src="assets/img/galeri1.jpg" alt="Repair">
+                <img src="assets/img/galeri2.jpg" alt="Repair">
+                <img src="assets/img/galeri3.jpg" alt="Repair">
+                <img src="assets/img/galeri4.jpg" alt="Repair">
+                <img src="assets/img/galeri5.jpg" alt="Repair">
+                <img src="assets/img/galeri6.jpg" alt="Repair">
+                <img src="assets/img/galeri7.jpg" alt="Repair">
+                <img src="assets/img/galeri8.jpg" alt="Repair">
+                <img src="assets/img/galeri9.jpg" alt="Repair">
+            </div>
+        </div>
+    </section>
+    <!-- Gallery End -->
+
+    <!-- Testimonial Start -->
+    <section class="testimonial" id="testimonial">
+        <h1>TESTI<span>MONIALS</span></h1>
+        <hr>
+        <div id="carouselTestimonial" class="carousel carousel-testimonial slide text-center" data-bs-ride="carousel">
+            <div class="carousel-inner mb-10">
+                <div class="carousel-item text-center text-wrap active">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@edwinbaskara</strong></h5>
+                    <p class="m-0 pt-3 text-wrap">Solusi sepatu kotor anda, Bersih, Wangi, Like a New.</p>
+                </div>
+                <div class="carousel-item text-center">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@assitanurbaiti</strong></h5>
+                    <p class="m-0 pt-3 text-wrap">Engga tau ini hasil tangan manusia yang mana dan yang seperti apa. Nice job af. Thankioo, Enggak nyesel samsek.</p>
+                </div>
+                <div class="carousel-item text-center">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@yayaas</strong></h5>
+                    <p class="m-0 pt-3">untuk membersihkan langkah-langkah kita dari masa lalu yang kelam, sepatu kita juga harus bersih! Jangan bimbang, ini buktinya cek @beshoescucisepatu.</p>
+                </div>
+                <div class="carousel-item text-center">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@rickysnggh</strong></h5>
+                    <p class="m-0 pt-3">terima kasih. tempat cuci sepatu yang affordable, for sure clean bisa COD lagi atat drop your address nanti dijemput sdama admin.</p>
+                </div>
+                <div class="carousel-item text-center">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@widarwatii_</strong></h5>
+                    <p class="m-0 pt-3">thx u bestie sudah bikin sepatuku kayak baru lagi.</p>
+                </div>
+                <div class="carousel-item text-center">
+                    <h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">@agus_kempling</strong></h5>
+                    <p class="m-0 pt-3">Sepatu lama terlihat baru kembali.</p>
+                </div>
+            </div>
+            <div class="container p-3">
+                <ol class="carousel-indicators position-static">
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="0" class="active"></li>
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="1"></li>
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="2"></li>
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="3"></li>
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="4"></li>
+                    <li data-bs-target="#carouselTestimonial" data-bs-slide-to="5"></li>
+                </ol>
+            </div>
+            <a class="carousel-control-prev" href="#carouselTestimonial" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselTestimonial" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
+        </div>
+
+    </section>
+    <!-- Testimonial Start -->
+
+    <!-- Contact Start -->
+    <div class="container" id="contact">
+        <div class="row">
+            <div class="col-md-7">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.780625196265!2d110.44120537486887!3d-7.70667439231098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5dc8fcc4f965%3A0xd7c940ff45d1a4a3!2sBeshoes!5e0!3m2!1sid!2sid!4v1688579835382!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-md-5">
+                <center><h4>Contact Us</h4></center>
+                <div class="mt-5">
+                    <div class="d-flex">
+                        <i class="ri-map-pin-line"></i>
+                        <p>Address: Jl Kaliurang Km 17,5 ruko no 3 selatan SMP N 1 Pakem Sleman</p>
+                    </div><hr>
+                    <div class="d-flex">
+                    <i class="ri-phone-fill"></i>
+                        <p>Contact: 0856-4362-0742</p>
+                    </div><hr>
+                    <div class="d-flex">
+                    <i class="ri-time-fill"></i>
+                        <p>Open: Mon - Sat : 10.00 - 18.00 WIB</p>
+                    </div><hr>
+                    <div class="d-flex">
+                    <i class="ri-instagram-fill"></i>
+                        <p>Instagram: @beshoescucisepatu</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact Start -->
+
+
     <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
